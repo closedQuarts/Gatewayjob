07aa64fdcf43f14c49811c745e7153661d4162a0
Efe Demirtaş <efedem21@hotmail.com>
2025-07-03 02:13:17 +0300

    Proje dosyası ekle.

---
+++ Gatewayjob/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+
+await app.UseOcelot();
+
+app.Run();
